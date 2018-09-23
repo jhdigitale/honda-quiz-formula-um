1377e29290f5ddf2b8c1da316b046aa7f78ae3cf
AdemirA <ademiralgusto@hotmail.com>
2018-09-23 16:27:56 -0300

    Change links, and up register form

--- gpquiz/resources/views/site/register.blade.php
+++ gpquiz/resources/views/site/register.blade.php
@@ -85,21 +85,21 @@
               }
             });*/
 
-            if($("input[name='nome']").val() <= 0){
+            if($("input[name='name']").val() <= 0){
               alert("Preencha corretamente o campo: " + $("input[name='nome']").attr("placeholder"));
-              $("input[name='nome']").focus();
+              $("input[name='name']").focus();
             } else if($("input[name='email']").val() <= 0){
               alert("Preencha corretamente o campo: " + $("input[name='email']").attr("placeholder"));
               $("input[name='email']").focus();
-            } else if($("input[name='matricula']").val() <= 0){
+            } else if($("input[name='register']").val() <= 0){
               alert("Preencha corretamente o campo: " + $("input[name='matricula']").attr("placeholder"));
-              $("input[name='matricula']").focus();
+              $("input[name='register']").focus();
             } else if($("input[name='cpf']").val() <= 0){
               alert("Preencha corretamente o campo: " + $("input[name='cpf']").attr("placeholder"));
               $("input[name='cpf']").focus();
-            }else if($("select[name='planta']").val() <= 0){
+            }else if($("select[name='local']").val() <= 0){
               alert("Preencha corretamente o campo: PLANTA");
-              $("select[name='planta']").focus();
+              $("select[name='local']").focus();
             } else if($("input[name='check']").is(':checked') != true){
               alert("Confirme a leitura do regulamento");
               $("input[name='check']").focus();
