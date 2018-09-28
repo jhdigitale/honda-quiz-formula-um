704884de8446de5319dde8f169995f5b8a1e7e64
AdemirA <ademiralgusto@hotmail.com>
2018-09-27 23:12:14 -0300

    Number errors

--- gpquiz/resources/views/layouts/footer.blade.php
+++ gpquiz/resources/views/layouts/footer.blade.php
@@ -1,12 +1,12 @@
 <!-- /.container-fluid-->
 <!-- /.content-wrapper-->
-<footer class="sticky-footer">
-  <div class="container">
-    <div class="text-center">
-      <small>GP Quiz</small>
-    </div>
-  </div>
-</footer>
+{{--<footer class="sticky-footer">--}}
+  {{--<div class="container">--}}
+    {{--<div class="text-center">--}}
+      {{--<small>GP Quiz</small>--}}
+    {{--</div>--}}
+  {{--</div>--}}
+{{--</footer>--}}
 <!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
   <i class="fa fa-angle-up"></i>
