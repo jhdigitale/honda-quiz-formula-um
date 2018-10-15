28629f7b9f92d7f1fe484f04dab25d504f9a0436
AdemirA <ademiralgusto@hotmail.com>
2018-10-15 20:59:19 -0300

    Changes

--- gpquiz/app/Http/Controllers/WinnerController.php
+++ gpquiz/app/Http/Controllers/WinnerController.php
@@ -117,7 +117,7 @@ class WinnerController extends Controller
 
             foreach ($question->answersToCorrect($question) as $answer){
 
-                if(count($respostasUsuarios) > 0 && ($posicaoQuestao + 1) < count($respostasUsuarios)){
+                if(count($respostasUsuarios) > 0 && ($posicaoQuestao) < count($respostasUsuarios)){
 
                     $respostilha = $respostasUsuarios[$posicaoQuestao];
 
