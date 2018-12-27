276599a30820953c1d127aff3a21fa6a1db93642
AdemirA <ademiralgusto@hotmail.com>
2018-12-27 17:51:28 -0200

    Change footer and header

--- gpquiz/resources/views/layout_2019.blade.php
+++ gpquiz/resources/views/layout_2019.blade.php
@@ -8,7 +8,7 @@
   <meta name="author" content="">
   <link rel="icon" href="../../favicon.ico">
 
-  <title>GP Quiz - Honda</title>
+  <title>Honda Brasil</title>
 
 
   <!-- Bootstrap core CSS -->
--- gpquiz/resources/views/layouts/rodape_2019.php
+++ gpquiz/resources/views/layouts/rodape_2019.php
@@ -1,9 +1,9 @@
 <div class="clearfix"></div>
 <footer class="footer">
- <div class="col-md-6">
+ <div class="col-md-12 center">
      <b>2018 @ Honda</b> - Todos os direitos Reservados
 </div>
-<div class="col-md-6 text-right">
+<!-- <div class="col-md-6 text-right">
   Bem Vindo
-</div>
+</div> -->
 </footer>
